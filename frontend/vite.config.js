a162cb2b30ef6b2bdc5f1f81e647728f20326306
mariann <cesinelisa@gmail.com>
2025-08-19 19:33:48 -0500

    fix(vite.config.js): set correct base path in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://melizadev.github.io/e-commerce/",
+  base: "/e-commerce/",
 });
