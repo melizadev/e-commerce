1fb52af7cef6522fb523ff9873284c3d885162dd
mariann <cesinelisa@gmail.com>
2025-08-21 18:48:33 -0500

    style(ShowResults.jsx): adjust responsive design of search results

--- src/components/navbar/ShowResults.jsx
+++ src/components/navbar/ShowResults.jsx
@@ -21,7 +21,7 @@ const ShowResults = ({ searchResults }) => {
         <h2 className="text-2xl font-bold text-gray-800">
           Resultado de tu busqueda:
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-3 p-3">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 items-start gap-3 p-3">
           {searchResults.map((product) => (
             <div key={product.id}>
               <img
