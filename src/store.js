b31fed5f7098e95cebf49a8bb0c67194e26950ae
mariann <cesinelisa@gmail.com>
2026-07-21 17:55:00 -0500

    style(store): format code for consistency and readability

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit'
-import cartReducer from './features/cartSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./features/cartSlice";
 
 export const store = configureStore({
   reducer: {
-    cart: cartReducer
-  }
-})
+    cart: cartReducer,
+  },
+  devtools: true,
+});
